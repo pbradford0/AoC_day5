93f596d4282bda176b35f7b021b8182567a5c4ab
pbradford0 <phil.w.bradford@gmail.com>
2016-11-05 19:25:31 -0400

    completed

--- day5.py
+++ day5.py
@@ -4,9 +4,22 @@
 import sys
 import re
 
-#def dupes(line)
- # for index in line:
-    
+def new_nice(filename):
+  input = open(filename, 'rU')
+  #count all the nice strings
+  nice_strings = 0
+  #create regex "rules" for each nice condition
+  re_2x2 = re.compile('.*(.)(.).*\\1\\2')
+  re_sammich = re.compile('.*(.)(.)\\1')
+  #loop through the strings and +1 nice string for each match
+  for line in input:
+    if re.match(re_2x2, line):
+      if re.match(re_sammich, line):
+        print line
+        nice_strings = nice_strings + 1
+  
+  return nice_strings
+
 
 def nice_calc(filename):
   input = open(filename, 'rU')
@@ -21,9 +34,7 @@ def nice_calc(filename):
     if re.match(re_vowels, line):
       if re.match(re_dubs, line):
         if not re.match(re_verboten, line):
-          print line
-      #if re.match(re_dubs, line):
-       #if not re.match(re_verboten, line):
+          #print line
           nice_strings = nice_strings + 1
   
   return nice_strings
@@ -35,6 +46,9 @@ def main():
 
   nice_string = nice_calc(sys.argv[1])
   print str(nice_string) + " strings are nice strings."
+  
+  nice_string = new_nice(sys.argv[1])
+  print "Following the revision, " str(nice_string) + " strings are now nice strings."
 
 if __name__ == '__main__':
   main()
