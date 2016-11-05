5c6389188f8b9c085931ab3916ed8439f8d9eb5c
pbradford0 <phil.w.bradford@gmail.com>
2016-11-04 20:58:37 -0400

    stuck again

--- day5.py
+++ day5.py
@@ -4,20 +4,24 @@
 import sys
 import re
 
+#def dupes(line)
+ # for index in line:
+    
+
 def nice_calc(filename):
   input = open(filename, 'rU')
   #count all the nice strings
   nice_strings = 0
   #create regex "rules" for each nice condition
-  re_vowels = re.compile('[aeiouAEIOU]([^aeiouAEIOU]*)[aeiouAEIOU]([^aeiouAEIOU]*)[aeiouAEIOU]')
-  re_dubs = re.compile('(.)\1')
-  re_verboten = re.compile('^(?!ab)(?!cd)(?!pq)(?!xy).*$')
+  re_vowels = re.compile('.*[aeiou].*[aeiou].*[aeiou]')
+  re_dubs = re.compile('.*(.)\\1')
+  re_verboten = re.compile('.*(ab)|(cd)|(pq)|(xy)')
   #loop through the strings and +1 nice string for each match
   for line in input:
-    if re.match(re_dubs, line):
-      #print line
+    if re.match(re_vowels, line) and re.match(re_dubs, line) and not re.match(re_verboten, line):
+      print line
       #if re.match(re_dubs, line):
-        #if re.match(re_verboten, line):
+       #if not re.match(re_verboten, line):
       nice_strings = nice_strings + 1
   
   return nice_strings
